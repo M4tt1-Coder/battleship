a13f635281aee39d3125e0d549d60f1c7b5f38b0
M4tt1-Coder <geimat75@gmail.com>
2025-10-26 18:22:03 +0100

    [ setup ] Updated setup with Gradle and JavaFX

---
+++ src/main/java/com/matti/battleship_game/Launcher.java
@@ -0,0 +1,9 @@
+package com.matti.battleship_game;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
