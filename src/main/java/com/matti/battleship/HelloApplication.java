1dd379104933d4027c01cead17d229c6d81d90e3
Thomas-Weigl <weigl.thomas@hotmail.com>
2025-10-30 16:44:29 +0100

    Adds Test1Application and improves HelloApplication

--- src/main/java/com/matti/battleship/HelloApplication.java
+++ src/main/java/com/matti/battleship/HelloApplication.java
@@ -1,19 +1,33 @@
 package com.matti.battleship;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Hello!");
+        Button btn = new Button("Click me");
+        btn.setOnAction( event ->
+                System.out.println("hello world")
+
+        );
+
+        StackPane root = new StackPane(); //Layout
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setScene(scene);
+        
+        primaryStage.show();
     }
 }
