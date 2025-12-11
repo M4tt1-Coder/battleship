535eb5ef00ccd150df053bb3b7c80ad89761985e
Thomas-Weigl <weigl.thomas@hotmail.com>
2025-12-11 16:00:28 +0100

    temp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.matti.battleship {
     requires com.almasb.fxgl.all;
     requires java.desktop;
     requires javafx.graphics;
-    requires java.desktop;
+    //requires java.desktop;
 
     opens com.matti.battleship to javafx.fxml;
     exports com.matti.battleship;
