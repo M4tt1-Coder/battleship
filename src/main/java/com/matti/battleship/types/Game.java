18856cf89773b9b5826e1cbb53073f21fcf5f086
M4tt1-Coder <geimat75@gmail.com>
2025-12-26 18:03:16 +0100

    [ refactor ] 'Game' class element functions

--- src/main/java/com/matti/battleship/types/Game.java
+++ src/main/java/com/matti/battleship/types/Game.java
@@ -2,11 +2,65 @@ package com.matti.battleship.types;
 
 import com.matti.battleship.enums.PlayingMode;
 
+/**
+ * Represents a Battleship game instance.
+ * Contains information about the playing mode, players, and game status.
+ * 
+ * @author m4tt1
+ */
 public class Game {
-    public boolean hasEnded = false;
-    private PlayingMode playingMode;
-    public Player playerA;
-    public Player playerB;
-    public Game() {
+    /**
+     * TRUE, when the game has ended; FALSE otherwise.
+     */
+    private boolean hasEnded = false;
+    
+    /**
+     * Playing mode of the game.
+     */
+    private final PlayingMode playingMode;
+    
+    /**
+     * Player instance representing the local player.
+     */
+    public Player player;
+    
+    /**
+     * Opponent player instance.
+     * Either another human player or an AI, depending on the playing mode.
+     */
+    public Player opponent;
+    
+    public Game(PlayingMode playingMode, Player player, Player opponent) {
+        this.playingMode = playingMode;
+        this.player = player;
+        this.opponent = opponent;
+        this.hasEnded = false;
+    }
+    
+    /**
+     * Retrieves the playing mode of the game.
+     * 
+     * @return Playing mode of the game.
+     */
+    public PlayingMode getPlayingMode() {
+        return playingMode;
+    }
+    
+    /**
+     * Checks if the game has ended.
+     * 
+     * @return TRUE, when the game has ended; FALSE otherwise.
+     */
+    public boolean hasEnded() {
+        return hasEnded;
+    }
+    
+    /**
+     * Sets the game as ended or not.
+     * 
+     * @param hasEnded TRUE, when the game has ended; FALSE otherwise.
+     */
+    public void setHasEnded(boolean hasEnded) {
+        this.hasEnded = hasEnded;
     }
 }
