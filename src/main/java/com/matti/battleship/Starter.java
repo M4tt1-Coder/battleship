31058105d25f0ab88d0b2770ddaf87d40d32c967
M4tt1-Coder <geimat75@gmail.com>
2025-12-28 20:13:38 +0100

    [ refactor ] test

--- src/main/java/com/matti/battleship/Starter.java
+++ src/main/java/com/matti/battleship/Starter.java
@@ -2,9 +2,6 @@ package com.matti.battleship;
 
 import javafx.application.Application;
 
-// TODO: Finish automated formatting of the codebase before every GIT commit -> husky pre-commit
-// hook
-
 public class Starter {
   public static void main(String[] args) {
     Application.launch(BattleshipApp.class, args);
