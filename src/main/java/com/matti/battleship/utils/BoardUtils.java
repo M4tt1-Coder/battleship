82f808d875e4f95742d2313085223a29140cccc9
Thomas-Weigl <weigl.thomas@hotmail.com>
2026-01-18 19:16:58 +0100

    Log4J configuration file parsing solved

--- src/main/java/com/matti/battleship/utils/BoardUtils.java
+++ src/main/java/com/matti/battleship/utils/BoardUtils.java
@@ -166,7 +166,7 @@ public class BoardUtils {
     ArrayList<ShipLength> initialShipSetup = new ArrayList<ShipLength>();
     // try to select an equal number of each ship size
 
-      while (numMandatoryFields > 0) {
+    while (numMandatoryFields > 0) {
       for (ShipLength length : ShipLength.values()) {
         if (numMandatoryFields - length.getValue() >= 0) {
           initialShipSetup.add(length);
